0a0d62445efc409220d91d1c9fbf74b3412f9f68
Arc Earth <ArcEarth@outlook.com>
2016-03-07 11:20:41 -0500

    delete private content

--- A4handout/calculateDET.m
+++
@@ -1,9 +0,0 @@
-function [hx,hy,tau] = calculateDET(featList, nFeatures, X,y)
-    sumAlpha = sum([featList.alpha]);
-    resp = evalBoosted(featList, nFeatures, X);
-    tau = (-1:0.001:1.01)' * sumAlpha;
-    hy = 1 - sum(bsxfun(@gt,resp(y>0),tau),2) ./ sum(y>0);
-    hx = 1 - sum(bsxfun(@lt,resp(y==0),tau),2) ./ sum(y==0);
-    hy(isnan(hy)) = 0;
-    hx(isnan(hx)) = 0;
-return;
--- A4handout/util/doAdaBoostStep.m
+++
@@ -1,7 +0,0 @@
-function wghts = doAdaBoostStep(wghts, alpha, H, y)
-  
-  ea = exp(alpha);
-  idx = (H ~= y);
-  wghts(idx) = wghts(idx) * ea;
- 
-  return;
